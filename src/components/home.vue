caa620050d1707f3de166446f548f8ca931c7575
Brieuc Lambert <lambert.brieuc@gmail.com>
2018-05-31 00:46:25 +0200

    improved forms

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,23 +1,67 @@
 <template>
     <div>
         <navbar></navbar>
-        <div class="row flex-center at-row">
-            <div class="col-lg-4" style="border: solid 1px black"> Grid 4</div>
-            <div class="col-lg-8" style="border: solid 1px black"> grid 8</div>
+        <div class="row login-row">
+            <div class="col-xs-24 col-sm-24 col-md-24 col-lg-24">
+                <div class="form-login-container">
+                    <div class="form-item">
+                        <h1>Login</h1>
+                    </div>
+                    <div class="form-item">
+                        <p class="form-label">Email</p>
+                        <at-input name="input" v-model="emailInput" v-on:change="checkEmailValidity"  placeholder="sarah@abtasty.com" :status="emailSuccess"></at-input>
+                    </div>
 
-      </div>
-  </div>
+                    <div class="form-item">
+                        <p class="form-label">Password</p>
+                        <at-input type="password" v-model="password" placeholder="azerty"></at-input>
+                    </div>
+
+                    <div class="form-item">
+                        <at-button type="primary" ref="button-loading" v-on:click="addLoadingAttribute" :loading="loading">Login</at-button>
+                        <router-link class="" to="/signup">
+                            <at-button type="text">Don't have an account yet?</at-button>
+                        </router-link>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-    import navbar from './navbar.vue'
-    export default {
-        components: {
-            navbar
-        }
+import navbar from './navbar.vue'
+export default {
+    components: {
+        navbar
     }
+};
 </script>
 
 <style>
-
+.login-row {
+    height: calc(100vh - 48px);
+}
+.form-login-container {
+    margin: auto;
+    padding-top: 40px;
+}
+.form-item{
+    width: 300px;
+    margin: 10px auto;
+}
+.form-item h1 {
+    text-align: center;
+}
+.form-label {
+    margin: 3px 5px;
+    text-align: center;
+}
+.left-login {
+    background-color: #6594E2;
+    width: 100%;
+    height: 100%;
+}
 </style>
---
+++ src/components/signup.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="signup">
+        <navbar></navbar>
+        <div class="row signup-row">
+            <div class="col-xs-24 col-sm-24 col-md-24 col-lg-24">
+                <div class="form-signup-container">
+                    <div class="form-item">
+                        <h1>Create an account</h1>
+                    </div>
+                    <div class="form-item">
+                        <p class="form-label">Email</p>
+                        <at-input name="input" v-model="emailInput" v-on:change="checkEmailValidity"  placeholder="sarah@abtasty.com" :status="emailSuccess"></at-input>
+                    </div>
+                    <div class="form-item">
+                        <p class="form-label">First Name</p>
+                        <at-input name="input"  placeholder="Sarah"></at-input>
+                    </div>
+                    <div class="form-item">
+                        <p class="form-label">Last Name</p>
+                        <at-input name="input"  placeholder="Miller"></at-input>
+                    </div>
+                    <div class="form-item">
+                        <p class="form-label">Password</p>
+                        <at-input type="password" v-model="password" placeholder="azerty"></at-input>
+                    </div>
+                    <div class="form-item">
+                        <p class="form-label">Password Confirmation</p>
+                        <at-input type="password" placeholder="azerty"></at-input>
+                    </div>
+                    <div class="form-item">
+                        <at-button type="primary" ref="button-loading" v-on:click="addLoadingAttribute" :loading="loading">Sign-up</at-button>
+
+                        <router-link class="" to="/">
+                            <at-button type="text">Already have an account</at-button>
+                        </router-link>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import navbar from './navbar.vue'
+export default {
+    components: {
+        navbar
+    },
+    data () {
+        return {
+            emailSuccess: null,
+            emailInput: null,
+            password: null,
+            loading: false
+        }
+    },
+    methods: {
+        emailValidity () {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(this.emailInput).toLowerCase());
+        },
+        checkEmailValidity () {
+            if (this.emailValidity()) {
+                this.emailSuccess = 'success'
+            } else {
+                this.emailSuccess = 'error'
+            }
+        },
+        addLoadingAttribute () {
+            this.loading = !this.loading;
+        }
+    }
+};
+</script>
+
+<style scoped>
+.signup-row {
+    height: calc(100vh - 48px);
+}
+.form-signup-container {
+    margin: auto;
+    padding-top: 40px;
+}
+.form-item {
+    width: 300px;
+    margin: 10px auto;
+}
+.form-item h1 {
+    text-align: center;
+}
+.form-label {
+    margin: 3px 5px;
+    text-align: center;
+}
+.left-signup {
+    background-color: #6594E2;
+    width: 100%;
+    height: 100%;
+}
+</style>
